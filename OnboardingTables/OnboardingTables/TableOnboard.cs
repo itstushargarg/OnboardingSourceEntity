33af1649f02a93a712973bc3a154d599efcd647f
Tushar Garg <tugar@microsoft.com>
2016-12-13 00:20:16 +0530

    Adding functions for creating stg and dbo tables and views

--- OnboardingTables/OnboardingTables/TableOnboard.cs
+++ OnboardingTables/OnboardingTables/TableOnboard.cs
@@ -18,12 +18,15 @@ namespace OnboardingTables
             InitializeComponent();
         }
 
-        public void CreateStgTable(string path)
+        private void AddSource()
         {
-            var p = new Microsoft.Build.Evaluation.Project(@"C:\Users\tugar\Source\Repos\Sales-IC-Datamg-AthenaDataManagement\DIDataManagement\DIDataManagement\DIDataManagement.sqlproj");
             //Adding a folder for new source
             //p.AddItem("Folder", @"C:\Users\tugar\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\test2");
             //Adding the file to the provided path
+        }
+        public void CreateStgTable(string pathi, Microsoft.Build.Evaluation.Project p)
+        {
+            string path = @"C:\Users\tugar\Source\Repos\Sales-IC-Datamg-AthenaDataManagement\DIDataManagement\DIDataManagement\stg\MSSales\Table\" + TableName.Text + ".sql";
             p.AddItem("Build", path);
             p.Save();
             //File.Create(path).Dispose();
@@ -35,17 +38,57 @@ namespace OnboardingTables
             }
         }
 
+        public void CreateStgView(string pathi, Microsoft.Build.Evaluation.Project p)
+        {
+            string path = @"C:\Users\tugar\Source\Repos\Sales-IC-Datamg-AthenaDataManagement\DIDataManagement\DIDataManagement\stg\MSSales\View\vw" + TableName.Text + ".sql";
+            p.AddItem("Build", path);
+            p.Save();
+            //File.Create(path).Dispose();
+            File.Create(path).Dispose();
+            using (TextWriter tw = new StreamWriter(path))
+            {
+                tw.WriteLine("CREATE VIEW [dbo].[vwAccountGrouping]\nAS\nSELECT [AccountGroupingID]\n      ,[AccountGroupingName]\n      ,[FiscalYear]\n      ,[ICDIUpdatedBy]\n      ,[ICDIETLRunID]\n      ,[ICDIIsLocked]\n      ,[ICDILockedTillDate]\n      ,[HashPK]\n      ,[HashNonPK]\n      ,SysStartTime\n      ,SysEndTime\n  FROM [dbo].[AccountGrouping] WITH(NOLOCK)");
+                tw.Close();
+            }
+        }
 
+        public void CreateDboTable(string pathi, Microsoft.Build.Evaluation.Project p)
+        {
+            string path = @"C:\Users\tugar\Source\Repos\Sales-IC-Datamg-AthenaDataManagement\DIDataManagement\DIDataManagement\dbo\MSSales\Table\" + TableName.Text + ".sql";
+            p.AddItem("Build", path);
+            p.Save();
+            //File.Create(path).Dispose();
+            File.Create(path).Dispose();
+            using (TextWriter tw = new StreamWriter(path))
+            {
+                tw.WriteLine("CREATE VIEW [dbo].[vwAccountGrouping]\nAS\nSELECT [AccountGroupingID]\n      ,[AccountGroupingName]\n      ,[FiscalYear]\n      ,[ICDIUpdatedBy]\n      ,[ICDIETLRunID]\n      ,[ICDIIsLocked]\n      ,[ICDILockedTillDate]\n      ,[HashPK]\n      ,[HashNonPK]\n      ,SysStartTime\n      ,SysEndTime\n  FROM [dbo].[AccountGrouping] WITH(NOLOCK)");
+                tw.Close();
+            }
+        }
+
+        public void CreateDboView(string pathi, Microsoft.Build.Evaluation.Project p)
+        {
+            string path = @"C:\Users\tugar\Source\Repos\Sales-IC-Datamg-AthenaDataManagement\DIDataManagement\DIDataManagement\dbo\MSSales\View\vw" + TableName.Text + ".sql";
+            p.AddItem("Build", path);
+            p.Save();
+            //File.Create(path).Dispose();
+            File.Create(path).Dispose();
+            using (TextWriter tw = new StreamWriter(path))
+            {
+                tw.WriteLine("CREATE VIEW [dbo].[vwAccountGrouping]\nAS\nSELECT [AccountGroupingID]\n      ,[AccountGroupingName]\n      ,[FiscalYear]\n      ,[ICDIUpdatedBy]\n      ,[ICDIETLRunID]\n      ,[ICDIIsLocked]\n      ,[ICDILockedTillDate]\n      ,[HashPK]\n      ,[HashNonPK]\n      ,SysStartTime\n      ,SysEndTime\n  FROM [dbo].[AccountGrouping] WITH(NOLOCK)");
+                tw.Close();
+            }
+        }
         private void Submit_Click(object sender, EventArgs e)
         {
             string path = @"C:\Users\tugar\Source\Repos\Sales-IC-Datamg-AthenaDataManagement\DIDataManagement\DIDataManagement\stg\MSSales\Table\" + TableName.Text + ".sql";
             if (!File.Exists(path))
             {
-
-                CreateStgTable(path);
-                //CreateStgView(path);
-                //CreateDboTable(path);
-                //CreateDboView(path);
+                var p = new Microsoft.Build.Evaluation.Project(@"C:\Users\tugar\Source\Repos\Sales-IC-Datamg-AthenaDataManagement\DIDataManagement\DIDataManagement\DIDataManagement.sqlproj");
+                CreateStgTable(path, p);
+                CreateStgView(path, p);
+                CreateDboTable(path, p);
+                CreateDboView(path, p);
             }
             else if (File.Exists(path))
             {
