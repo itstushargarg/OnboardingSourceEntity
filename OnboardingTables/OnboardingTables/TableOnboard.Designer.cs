82a7f28f0a3d710be9398ebf91b26ffdbb00bc76
Tushar Garg <tugar@microsoft.com>
2016-12-29 17:44:21 +0530

    Addinf Layout changes and adding function to add the table name to TemporalSystemVersioning if its temporal

--- OnboardingTables/OnboardingTables/TableOnboard.Designer.cs
+++ OnboardingTables/OnboardingTables/TableOnboard.Designer.cs
@@ -50,10 +50,6 @@
             this.GetTableDetails = new System.Windows.Forms.Button();
             this.AddScript = new System.Windows.Forms.Button();
             this.ChefScriptLabel = new System.Windows.Forms.Label();
-            this.ProcessIDLabel = new System.Windows.Forms.Label();
-            this.CatalogID = new System.Windows.Forms.TextBox();
-            this.CatalogIDLabel = new System.Windows.Forms.Label();
-            this.ProcessID = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.ColumnList = new System.Windows.Forms.CheckedListBox();
             this.PrimaryKeyColumns = new System.Windows.Forms.CheckedListBox();
@@ -62,6 +58,10 @@
             this.TemporalTableCheck = new System.Windows.Forms.CheckBox();
             this.FiscalYearCheck = new System.Windows.Forms.CheckBox();
             this.ScriptName = new System.Windows.Forms.ComboBox();
+            this.ProcessIDLabel = new System.Windows.Forms.Label();
+            this.CatalogID = new System.Windows.Forms.TextBox();
+            this.CatalogIDLabel = new System.Windows.Forms.Label();
+            this.ProcessID = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Submit
@@ -77,7 +77,7 @@
             // 
             // TargetTableName
             // 
-            this.TargetTableName.Location = new System.Drawing.Point(431, 155);
+            this.TargetTableName.Location = new System.Drawing.Point(431, 191);
             this.TargetTableName.Margin = new System.Windows.Forms.Padding(2);
             this.TargetTableName.Name = "TargetTableName";
             this.TargetTableName.Size = new System.Drawing.Size(93, 20);
@@ -87,7 +87,7 @@
             // TargetTableNameLabel
             // 
             this.TargetTableNameLabel.AutoSize = true;
-            this.TargetTableNameLabel.Location = new System.Drawing.Point(319, 158);
+            this.TargetTableNameLabel.Location = new System.Drawing.Point(319, 194);
             this.TargetTableNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TargetTableNameLabel.Name = "TargetTableNameLabel";
             this.TargetTableNameLabel.Size = new System.Drawing.Size(102, 13);
@@ -164,7 +164,7 @@
             // SourceTableNameLabel
             // 
             this.SourceTableNameLabel.AutoSize = true;
-            this.SourceTableNameLabel.Location = new System.Drawing.Point(13, 158);
+            this.SourceTableNameLabel.Location = new System.Drawing.Point(13, 194);
             this.SourceTableNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.SourceTableNameLabel.Name = "SourceTableNameLabel";
             this.SourceTableNameLabel.Size = new System.Drawing.Size(105, 13);
@@ -173,7 +173,7 @@
             // 
             // SourceSchemaName
             // 
-            this.SourceSchemaName.Location = new System.Drawing.Point(132, 155);
+            this.SourceSchemaName.Location = new System.Drawing.Point(132, 191);
             this.SourceSchemaName.Margin = new System.Windows.Forms.Padding(2);
             this.SourceSchemaName.Name = "SourceSchemaName";
             this.SourceSchemaName.Size = new System.Drawing.Size(51, 20);
@@ -184,7 +184,7 @@
             // ServerNameLabel
             // 
             this.ServerNameLabel.AutoSize = true;
-            this.ServerNameLabel.Location = new System.Drawing.Point(13, 128);
+            this.ServerNameLabel.Location = new System.Drawing.Point(13, 162);
             this.ServerNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ServerNameLabel.Name = "ServerNameLabel";
             this.ServerNameLabel.Size = new System.Drawing.Size(72, 13);
@@ -193,7 +193,7 @@
             // 
             // ServerName
             // 
-            this.ServerName.Location = new System.Drawing.Point(132, 128);
+            this.ServerName.Location = new System.Drawing.Point(132, 162);
             this.ServerName.Margin = new System.Windows.Forms.Padding(2);
             this.ServerName.Name = "ServerName";
             this.ServerName.Size = new System.Drawing.Size(158, 20);
@@ -203,7 +203,7 @@
             // DatabaseNameLabel
             // 
             this.DatabaseNameLabel.AutoSize = true;
-            this.DatabaseNameLabel.Location = new System.Drawing.Point(319, 128);
+            this.DatabaseNameLabel.Location = new System.Drawing.Point(319, 162);
             this.DatabaseNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.DatabaseNameLabel.Name = "DatabaseNameLabel";
             this.DatabaseNameLabel.Size = new System.Drawing.Size(87, 13);
@@ -212,7 +212,7 @@
             // 
             // DatabaseName
             // 
-            this.DatabaseName.Location = new System.Drawing.Point(431, 125);
+            this.DatabaseName.Location = new System.Drawing.Point(431, 159);
             this.DatabaseName.Margin = new System.Windows.Forms.Padding(2);
             this.DatabaseName.Name = "DatabaseName";
             this.DatabaseName.Size = new System.Drawing.Size(93, 20);
@@ -221,7 +221,7 @@
             // 
             // SourceTableName
             // 
-            this.SourceTableName.Location = new System.Drawing.Point(187, 155);
+            this.SourceTableName.Location = new System.Drawing.Point(187, 191);
             this.SourceTableName.Margin = new System.Windows.Forms.Padding(2);
             this.SourceTableName.Name = "SourceTableName";
             this.SourceTableName.Size = new System.Drawing.Size(103, 20);
@@ -230,7 +230,7 @@
             // 
             // GetTableDetails
             // 
-            this.GetTableDetails.Location = new System.Drawing.Point(552, 151);
+            this.GetTableDetails.Location = new System.Drawing.Point(552, 187);
             this.GetTableDetails.Margin = new System.Windows.Forms.Padding(2);
             this.GetTableDetails.Name = "GetTableDetails";
             this.GetTableDetails.Size = new System.Drawing.Size(99, 26);
@@ -259,42 +259,6 @@
             this.ChefScriptLabel.Size = new System.Drawing.Size(62, 13);
             this.ChefScriptLabel.TabIndex = 23;
             this.ChefScriptLabel.Text = "Chef Script:";
-            // 
-            // ProcessIDLabel
-            // 
-            this.ProcessIDLabel.AutoSize = true;
-            this.ProcessIDLabel.Location = new System.Drawing.Point(319, 189);
-            this.ProcessIDLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.ProcessIDLabel.Name = "ProcessIDLabel";
-            this.ProcessIDLabel.Size = new System.Drawing.Size(62, 13);
-            this.ProcessIDLabel.TabIndex = 29;
-            this.ProcessIDLabel.Text = "Process ID:";
-            // 
-            // CatalogID
-            // 
-            this.CatalogID.Location = new System.Drawing.Point(132, 189);
-            this.CatalogID.Margin = new System.Windows.Forms.Padding(2);
-            this.CatalogID.Name = "CatalogID";
-            this.CatalogID.Size = new System.Drawing.Size(158, 20);
-            this.CatalogID.TabIndex = 28;
-            // 
-            // CatalogIDLabel
-            // 
-            this.CatalogIDLabel.AutoSize = true;
-            this.CatalogIDLabel.Location = new System.Drawing.Point(13, 189);
-            this.CatalogIDLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.CatalogIDLabel.Name = "CatalogIDLabel";
-            this.CatalogIDLabel.Size = new System.Drawing.Size(60, 13);
-            this.CatalogIDLabel.TabIndex = 27;
-            this.CatalogIDLabel.Text = "Catalog ID:";
-            // 
-            // ProcessID
-            // 
-            this.ProcessID.Location = new System.Drawing.Point(431, 186);
-            this.ProcessID.Margin = new System.Windows.Forms.Padding(2);
-            this.ProcessID.Name = "ProcessID";
-            this.ProcessID.Size = new System.Drawing.Size(93, 20);
-            this.ProcessID.TabIndex = 26;
             // 
             // label1
             // 
@@ -372,11 +336,51 @@
             this.ScriptName.Size = new System.Drawing.Size(274, 21);
             this.ScriptName.TabIndex = 35;
             // 
+            // ProcessIDLabel
+            // 
+            this.ProcessIDLabel.AutoSize = true;
+            this.ProcessIDLabel.Location = new System.Drawing.Point(319, 133);
+            this.ProcessIDLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ProcessIDLabel.Name = "ProcessIDLabel";
+            this.ProcessIDLabel.Size = new System.Drawing.Size(62, 13);
+            this.ProcessIDLabel.TabIndex = 40;
+            this.ProcessIDLabel.Text = "Process ID:";
+            // 
+            // CatalogID
+            // 
+            this.CatalogID.Location = new System.Drawing.Point(132, 130);
+            this.CatalogID.Margin = new System.Windows.Forms.Padding(2);
+            this.CatalogID.Name = "CatalogID";
+            this.CatalogID.Size = new System.Drawing.Size(158, 20);
+            this.CatalogID.TabIndex = 39;
+            // 
+            // CatalogIDLabel
+            // 
+            this.CatalogIDLabel.AutoSize = true;
+            this.CatalogIDLabel.Location = new System.Drawing.Point(13, 130);
+            this.CatalogIDLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.CatalogIDLabel.Name = "CatalogIDLabel";
+            this.CatalogIDLabel.Size = new System.Drawing.Size(60, 13);
+            this.CatalogIDLabel.TabIndex = 38;
+            this.CatalogIDLabel.Text = "Catalog ID:";
+            // 
+            // ProcessID
+            // 
+            this.ProcessID.Location = new System.Drawing.Point(431, 127);
+            this.ProcessID.Margin = new System.Windows.Forms.Padding(2);
+            this.ProcessID.Name = "ProcessID";
+            this.ProcessID.Size = new System.Drawing.Size(93, 20);
+            this.ProcessID.TabIndex = 37;
+            // 
             // TableOnboarding
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(826, 473);
+            this.Controls.Add(this.ProcessIDLabel);
+            this.Controls.Add(this.CatalogID);
+            this.Controls.Add(this.CatalogIDLabel);
+            this.Controls.Add(this.ProcessID);
             this.Controls.Add(this.FiscalYearCheck);
             this.Controls.Add(this.TemporalTableCheck);
             this.Controls.Add(this.ScriptName);
@@ -385,10 +389,6 @@
             this.Controls.Add(this.PrimaryKeyLabel);
             this.Controls.Add(this.ColumnList);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.ProcessIDLabel);
-            this.Controls.Add(this.CatalogID);
-            this.Controls.Add(this.CatalogIDLabel);
-            this.Controls.Add(this.ProcessID);
             this.Controls.Add(this.AddScript);
             this.Controls.Add(this.ChefScriptLabel);
             this.Controls.Add(this.GetTableDetails);
@@ -439,9 +439,6 @@
         private System.Windows.Forms.Button GetTableDetails;
         private System.Windows.Forms.Button AddScript;
         private System.Windows.Forms.Label ChefScriptLabel;
-        private System.Windows.Forms.Label ProcessIDLabel;
-        private System.Windows.Forms.Label CatalogIDLabel;
-        private System.Windows.Forms.TextBox ProcessID;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckedListBox ColumnList;
         private System.Windows.Forms.CheckedListBox PrimaryKeyColumns;
@@ -449,8 +446,11 @@
         private System.Windows.Forms.Button SelectPrimaryKey;
         private System.Windows.Forms.CheckBox TemporalTableCheck;
         private System.Windows.Forms.CheckBox FiscalYearCheck;
-        public System.Windows.Forms.TextBox CatalogID;
         public System.Windows.Forms.ComboBox ScriptName;
+        private System.Windows.Forms.Label ProcessIDLabel;
+        public System.Windows.Forms.TextBox CatalogID;
+        private System.Windows.Forms.Label CatalogIDLabel;
+        private System.Windows.Forms.TextBox ProcessID;
     }
 }
 
