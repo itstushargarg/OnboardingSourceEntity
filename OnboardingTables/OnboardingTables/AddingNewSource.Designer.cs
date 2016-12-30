f6871ed38da8fa9d31bfc8d26d0a19587f6b730b
Tushar Garg <tugar@microsoft.com>
2016-12-30 12:22:38 +0530

    Adding changes to the form AddingNewSource

--- OnboardingTables/OnboardingTables/AddingNewSource.Designer.cs
+++ OnboardingTables/OnboardingTables/AddingNewSource.Designer.cs
@@ -31,6 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.NewSourceName = new System.Windows.Forms.TextBox();
             this.Submit = new System.Windows.Forms.Button();
+            this.ConnectionString = new System.Windows.Forms.TextBox();
+            this.ConnectionStringLabel = new System.Windows.Forms.Label();
+            this.FrequencyNumber = new System.Windows.Forms.TextBox();
+            this.FrequencyNumberLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -44,14 +48,14 @@
             // 
             // NewSourceName
             // 
-            this.NewSourceName.Location = new System.Drawing.Point(112, 37);
+            this.NewSourceName.Location = new System.Drawing.Point(136, 37);
             this.NewSourceName.Name = "NewSourceName";
             this.NewSourceName.Size = new System.Drawing.Size(100, 20);
             this.NewSourceName.TabIndex = 1;
             // 
             // Submit
             // 
-            this.Submit.Location = new System.Drawing.Point(80, 83);
+            this.Submit.Location = new System.Drawing.Point(33, 199);
             this.Submit.Name = "Submit";
             this.Submit.Size = new System.Drawing.Size(75, 23);
             this.Submit.TabIndex = 2;
@@ -59,11 +63,47 @@
             this.Submit.UseVisualStyleBackColor = true;
             this.Submit.Click += new System.EventHandler(this.Submit_Click);
             // 
+            // ConnectionString
+            // 
+            this.ConnectionString.Location = new System.Drawing.Point(136, 84);
+            this.ConnectionString.Name = "ConnectionString";
+            this.ConnectionString.Size = new System.Drawing.Size(641, 20);
+            this.ConnectionString.TabIndex = 4;
+            // 
+            // ConnectionStringLabel
+            // 
+            this.ConnectionStringLabel.AutoSize = true;
+            this.ConnectionStringLabel.Location = new System.Drawing.Point(30, 87);
+            this.ConnectionStringLabel.Name = "ConnectionStringLabel";
+            this.ConnectionStringLabel.Size = new System.Drawing.Size(91, 13);
+            this.ConnectionStringLabel.TabIndex = 3;
+            this.ConnectionStringLabel.Text = "ConnectionString:";
+            // 
+            // FrequencyNumber
+            // 
+            this.FrequencyNumber.Location = new System.Drawing.Point(136, 131);
+            this.FrequencyNumber.Name = "FrequencyNumber";
+            this.FrequencyNumber.Size = new System.Drawing.Size(100, 20);
+            this.FrequencyNumber.TabIndex = 6;
+            // 
+            // FrequencyNumberLabel
+            // 
+            this.FrequencyNumberLabel.AutoSize = true;
+            this.FrequencyNumberLabel.Location = new System.Drawing.Point(30, 134);
+            this.FrequencyNumberLabel.Name = "FrequencyNumberLabel";
+            this.FrequencyNumberLabel.Size = new System.Drawing.Size(97, 13);
+            this.FrequencyNumberLabel.TabIndex = 5;
+            this.FrequencyNumberLabel.Text = "FrequencyNumber:";
+            // 
             // AddingNewSource
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 130);
+            this.ClientSize = new System.Drawing.Size(811, 251);
+            this.Controls.Add(this.FrequencyNumber);
+            this.Controls.Add(this.FrequencyNumberLabel);
+            this.Controls.Add(this.ConnectionString);
+            this.Controls.Add(this.ConnectionStringLabel);
             this.Controls.Add(this.Submit);
             this.Controls.Add(this.NewSourceName);
             this.Controls.Add(this.label1);
@@ -79,5 +119,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox NewSourceName;
         private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.TextBox ConnectionString;
+        private System.Windows.Forms.Label ConnectionStringLabel;
+        private System.Windows.Forms.TextBox FrequencyNumber;
+        private System.Windows.Forms.Label FrequencyNumberLabel;
     }
 }
