9d4c71cf6f35ccb4fcf1454634885fa4d14c276e
Tushar Garg <tugar@microsoft.com>
2016-12-30 12:00:09 +0530

    Refining code for AddToProcessMonitor()

--- OnboardingTables/OnboardingTables/TableOnboard.cs
+++ OnboardingTables/OnboardingTables/TableOnboard.cs
@@ -437,37 +437,36 @@ namespace OnboardingTables
         {
             //Checking if Catalog ID exists in the Table ProcessMonitor
             var connectionString = String.Format("Data Source=AZICDEVDISQL1;Initial Catalog=CHEF;Integrated Security=True;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=True");
+            SqlDataReader reader;
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string[] restrictions = new string[4] { null, SourceSchemaName.Text, SourceTableName.Text, null };
+                string[] restrictions = new string[4] { null, "CHEF", "Configuration", null };
+                string queryString = String.Format("SELECT CatalogID FROM CHEF.ProcessMonitor Where CatalogID = {0};", CatalogID.Text);
+                SqlCommand command = new SqlCommand(queryString, connection);
                 connection.Open();
-                sourceTableColumns = connection.GetSchema("Columns", restrictions).Rows;
-
-                foreach (System.Data.DataRow rowColumn in sourceTableColumns)
+                reader = command.ExecuteReader();
+                if (!reader.HasRows)
                 {
-                    var ColumnName = (rowColumn[3].ToString());
+                    String file = ChefSqlProjpath.Replace("CHEF.sqlproj", "Scripts\\Post-Deployment\\CHEF_ProcessMonitor_insert.sql");
+                    string data = String.Format("\nUNION ALL");
+                    data += String.Format("\nSELECT SourceMasterID, {0}, GETUTCDATE() , NULL FROM  [CHEF].[SourceMaster] WITH (NOLOCK) WHERE Sourcename ='{1}'", CatalogID.Text, SourceName.Text);
+                    string text = File.ReadAllText(file);
+                    int lastindex = text.LastIndexOf("Sourcename", StringComparison.OrdinalIgnoreCase);
+                    lastindex += 10;
+                    //Finding index of next newline character
+                    while (text[lastindex] != '\n')
+                    {
+                        lastindex++;
+                    }
+                    text = text.Insert(lastindex, data);
+                    using (TextWriter tw = new StreamWriter(file))
+                    {
+                        tw.Write(text);
+                        tw.Close();
+                    }
                 }
+                reader.Close();
                 connection.Close();
-            }
-            if (true)
-            {
-                String file = ChefSqlProjpath.Replace("CHEF.sqlproj", "Scripts\\Post-Deployment\\CHEF_ProcessMonitor_insert.sql");
-                string data = String.Format("\nUNION ALL");
-                data += String.Format("\nSELECT SourceMasterID, {0}, GETUTCDATE() , NULL FROM  [CHEF].[SourceMaster] WITH (NOLOCK) WHERE Sourcename ='{1}'", CatalogID.Text, SourceName.Text);
-                string text = File.ReadAllText(file);
-                int lastindex = text.LastIndexOf("Sourcename", StringComparison.OrdinalIgnoreCase);
-                lastindex += 10;
-                //Finding index of next newline character
-                while (text[lastindex] != '\n')
-                {
-                    lastindex++;
-                }
-                text = text.Insert(lastindex, data);
-                using (TextWriter tw = new StreamWriter(file))
-                {
-                    tw.Write(text);
-                    tw.Close();
-                }
             }
         }
         public void Submit_Click(object sender, EventArgs e)
