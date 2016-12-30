7816e0c4690850e617aaa4f876e1e67e8b2d97f7
Tushar Garg <tugar@microsoft.com>
2016-12-30 20:49:03 +0530

    Refactoring Code

--- OnboardingTables/OnboardingTables/AddingNewSource.cs
+++ OnboardingTables/OnboardingTables/AddingNewSource.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-
+using System.Data.SqlClient;
 
 namespace OnboardingTables
 {
@@ -21,7 +21,12 @@ namespace OnboardingTables
             InitializeComponent();
         }
 
-        private void Submit_Click(object sender, EventArgs e)
+        public void AddConnectionStringToChef()
+        {
+
+        }
+
+        public void AddFoldersForNewSource()
         {
             SourceName = this.NewSourceName.Text;
             String SourcePath = TableOnboarding.SqlProjpath;
@@ -63,6 +68,73 @@ namespace OnboardingTables
             p.AddItem("Folder", FunctionsPath);
             p.AddItem("Folder", stgFunctionsPath);
             p.Save();
+        }
+
+        public void AddToSourceMaster()
+        {
+            String file = TableOnboarding.ChefSqlProjpath.Replace("CHEF.sqlproj", "Scripts\\Post-Deployment\\CHEF_SourceMaster_Insert.sql");
+            string data = String.Format("\n,('{0}'\t\t\t\t\t,1440	, DATEADD(MINUTE,1740,CONVERT(DATETIME, CONVERT(VARCHAR, GETDATE(), 101))) , 1 , 0 , 0 , NULL , DATEADD( MINUTE , -1440 , DATEADD(dd, 0, DATEDIFF(dd, 0, GETUTCDATE())) ),1);", tob.SourceName.Text);
+            string text = File.ReadAllText(file);
+            int lastindex = text.LastIndexOf("GETUTCDATE()", StringComparison.OrdinalIgnoreCase);
+            lastindex += 12;
+            //Finding index of next newline character
+            while (text[lastindex] != '\n')
+            {
+                lastindex++;
+            }
+            text = text.Insert(lastindex, data);
+            using (TextWriter tw = new StreamWriter(file))
+            {
+                tw.Write(text);
+                tw.Close();
+            }
+        }
+        public void AddToProcessMonior()
+        {
+            //Checking if Catalog ID exists in the Table ProcessMonitor
+            var connectionString = String.Format("Data Source=AZICDEVDISQL1;Initial Catalog=CHEF;Integrated Security=True;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=True");
+            SqlDataReader reader;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string[] restrictions = new string[4] { null, "CHEF", "Configuration", null };
+                string queryString = String.Format("SELECT CatalogID FROM CHEF.ProcessMonitor Where CatalogID = {0};", tob.CatalogID.Text);
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                reader = command.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    String file = TableOnboarding.ChefSqlProjpath.Replace("CHEF.sqlproj", "Scripts\\Post-Deployment\\CHEF_ProcessMonitor_insert.sql");
+                    string data = String.Format("\nUNION ALL");
+                    data += String.Format("\nSELECT SourceMasterID, {0}, GETUTCDATE() , NULL FROM  [CHEF].[SourceMaster] WITH (NOLOCK) WHERE Sourcename ='{1}'", tob.CatalogID.Text, tob.SourceName.Text);
+                    string text = File.ReadAllText(file);
+                    int lastindex = text.LastIndexOf("Sourcename", StringComparison.OrdinalIgnoreCase);
+                    lastindex += 10;
+                    //Finding index of next newline character
+                    while (text[lastindex] != '\n')
+                    {
+                        lastindex++;
+                    }
+                    text = text.Insert(lastindex, data);
+                    using (TextWriter tw = new StreamWriter(file))
+                    {
+                        tw.Write(text);
+                        tw.Close();
+                    }
+                }
+                reader.Close();
+                connection.Close();
+            }
+        }
+        public void AddSqlcmdVariable()
+        {
+
+        }
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            AddFoldersForNewSource();
+            AddConnectionStringToChef();
+            AddToProcessMonior();
+            AddToSourceMaster();
             tob.ListofSources();
             this.Close();
             MessageBox.Show("The Source " + SourceName + " Successfully added");
